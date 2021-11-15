061983328d7ed8313a30ddf56014407f2b8f79ef
Mel Incognito <melxincognito@gmail.com>
2021-11-14 19:38:52 -0700

    getting shit done

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,27 +1,52 @@
 import React from 'react';
 import Card from '../ui/Card'
 import classes from './NewMeetupsForm.module.css'
+import { useRef } from 'react';
 
+function NewMeetupForm(props) {
 
-function NewMeetupForm() {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enterdDescription = descriptionInputRef.current.value;
+
+        const meetupData = {
+            title: enteredTitle, 
+            image: enteredImage,
+            address: enteredAddress,
+            description: enterdDescription,
+        }
+
+        props.onAddMeetup(meetupData)
+
+    }
+
     return( 
     <Card>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
                 <label htmlFor='title'>Título de Grupo</label>
-                <input type='text' required id='title'/>
+                <input type='text' required id='title' ref={titleInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='image'>Imágen</label>
-                <input type='url' required id='image'/>
+                <input type='url' required id='image' ref={imageInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='address'>Direción</label>
-                <input type='text' required id='address'/>
+                <input type='text' required id='address' ref={addressInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='description'>Resumen de Grupo</label>
-                <textarea id='description' required rows='5'> </textarea>
+                <textarea id='description' required rows='5' ref={descriptionInputRef} />
             </div>
             <div className={classes.actions}> 
                 <button> Añadir Grupo </button>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetup() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-de625-default-rtdb.firebaseio.com/meetups.json', 
+            {
+                method: 'POST', 
+                body: JSON.stringify(meetupData), 
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/')
+        });
+    }
 
     return( 
         <section> 
             <h1> Añadir Nuevo Grupo de Geek en España </h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
         
     )
